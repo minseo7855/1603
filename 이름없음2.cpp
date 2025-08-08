cd54bfa5caf3dc66b9b08bf3672ff5f61d3404ed
minseo7855 <kwonminseo7855@gmail.com>
2025-08-08 12:22:32 +0900

    Add files via upload

---
+++ 이름없음2.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(void)  {
+	int i, n, facto = 1;
+	printf("n = ?");
+	scanf("%d" , &n);
+	int i;
+	for(i = 1; i <=n; i++) {
+       facto= factto * i;
+	}
+	printf("factorial: %d /n", facto);
+	getchar();
+	rerurn 0;
+}
